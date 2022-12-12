e3d5ab59ea98d5041590a76d41d955b51b86711a
aimedivin <aimedifi003@gmail.com>
2022-12-12 13:34:18 -0800

    The success combination in business is

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: prints all possible different combinations of two digits
+ * Return: 0
+ */
+
+int main(void)
+{
+	int num = 1, num2 = 0, num3 = 2;
+
+	while (num < 90)
+	{
+		if (num3 % 10 == 0)
+		{
+			num2++;
+			num++;
+			num3 += (num + 1);
+		}
+		putchar((num2 % 10) + '0');
+		putchar((num % 10) + '0');
+		putchar((num3 % 10) + '0');
+
+		if (num < 89)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
+
