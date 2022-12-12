849b0281d0d8e91619cbe4179f0dafa239b6e827
aimedivin <aimedifi003@gmail.com>
2022-12-12 14:51:43 -0800

    The success combination in business is

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,13 +10,13 @@ int main(void)
 {
 	int num = 0, num2 = 1, num3 = 2;
 
-	while (num <= 6)
+	while (num <= 7)
 	{
 		num2 = num + 1;
 		while (num2 <= 8)
 		{
 			num3 = num2 + 1;
-			while (num3 <= 10)
+			while (num3 < 10)
 			{
 				putchar(num % 10 + '0');
 				putchar(num2 % 10 + '0');
