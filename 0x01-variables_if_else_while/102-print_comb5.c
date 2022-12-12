34058738a67f2509e9da80fbd5afa1e803139a0e
aimedivin <aimedifi003@gmail.com>
2022-12-12 15:36:11 -0800

    Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description:  prints all possible combinations of two two-digit numbers
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i, j, k, l, m;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 8; j++)
+		{
+			for (k = 0; k <= 9; k++)
+			{
+				putchar(i % 10 + '0');
+				putchar(j % 10 + '0');
+				putchar(' ');
+				for (l = i; l <= 9; l++)
+				{
+					putchar(l % 10 + '0');
+				}
+				for (m = j; m <= 9; m++)
+				{
+					putchar(m % 10 + '0');
+				}
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	return (0);
+}
