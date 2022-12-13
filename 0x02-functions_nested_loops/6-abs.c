d696047306b8390dfa04320894a825bc7fc9fa1a
aimedivin <aimedifi003@gmail.com>
2022-12-13 13:05:30 -0800

    no such thing as absolute value in this world

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs -  computes the absolute value of an integer
+ * @m: parameter of an integer type
+ * Return: negative or positive
+ */
+
+int _abs(int m)
+{
+	if (m >= 0)
+		return (m);
+	else
+		return (-m);
+}
+
