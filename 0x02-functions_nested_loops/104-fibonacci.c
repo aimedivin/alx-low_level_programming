2305a124ff3435689977fad8d8b76a19ecf4713d
aimedivin <aimedifi003@gmail.com>
2022-12-13 16:09:41 -0800

    prints the first 98 Fibonacci numbers

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description:  prints the first 98 Fibonacci numbers
+ * Return: 0
+ */
+
+int main(void)
+{
+	unsigned long fibbo1 = 0, fibbo2 = 1, sum;
+	int i = 0;
+
+	while (i < 98)
+	{
+		sum = fibbo1 + fibbo2;
+		printf("%lu, ", sum);
+
+		fibbo1 = fibbo2;
+		fibbo2 = sum;
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
