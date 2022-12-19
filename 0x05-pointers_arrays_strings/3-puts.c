301339754cb7bdd0a311ccda854c34191f3c090e
aimedivin <aimedifi003@gmail.com>
2022-12-19 12:42:46 -0800

    4. I can only go one way. I've not got a reverse gear

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _puts -  prints a string, followed by a new line, to stdout
+ * @str: a character pointer
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	puts(str);
+}
