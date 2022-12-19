d085f9dc23a26179e22a54fba226696495492a88
aimedivin <aimedifi003@gmail.com>
2022-12-19 12:20:15 -0800

    2. This report, by its very length, defends itself against the risk of being read

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * void swap_int(int *a, int *b) - swaps the values of two integers
+ * swap_int - swaps the values of two integers
  * @a: first integer pointer
  * @b: second integer pointer
  * Return: void
---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: a character pointer
+ * Return: an integer
+ */
+
+int _strlen(char *s)
+{
+	return (strlen(*s));
+}
