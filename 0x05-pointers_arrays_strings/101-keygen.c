9e98a8f7b68b1e3bdde7da2d4e52bf07abb7a0cf
aimedivin <aimedifi003@gmail.com>
2022-12-20 12:38:27 -0800

    11. Don't hate the hacker, hate the code

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - entry point
+ * Description:
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i, j, sum;
+	char psswrd[30];
+
+	srand(time(NULL));
+	sum = 0;
+	for (i = 0; i < 30; i++)
+	{
+		psswrd[i] = 33 + rand() % 91;
+		printf("%c", psswrd[i]);
+	}
+	return (0);
+}
