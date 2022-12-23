b08c8152b6bd9b9794ba4a8b2f02f85140b448b3
aimedivin <aimedifi003@gmail.com>
2022-12-23 13:09:10 -0800

    5. Always look up

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -13,7 +13,7 @@ char *string_toupper(char *str)
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		if (str[i] >= 'a' && str[i] <= 'z')
-			str[i] =  str[i] - 'a' - 'A';
+			str[i] =  str[i] - ('a' - 'A');
 	}
 	return (str);
 }
