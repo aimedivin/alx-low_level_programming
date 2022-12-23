54b89878f97eeb1361c6f8bfc2196cb26cc640db
aimedivin <aimedifi003@gmail.com>
2022-12-23 05:37:44 -0800

    function that works exactly like strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -13,7 +13,7 @@ int _strcmp(char *s1, char *s2)
 
 	for  (i = 0; s1[i] == s2[i]; i++)
 	{
-		if (s1 == '\0' && s2 == '\0')
+		if (s1[i] == '\0' && s2[i] == '\0')
 			return (0);
 	}
 	return (*s2 - *s1);
