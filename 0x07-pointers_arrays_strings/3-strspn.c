c65270645f7ba5c0c69283dfead674d263e7c4b1
aimedivin <aimedifi003@gmail.com>
2023-01-07 03:57:13 -0800

    strspn in reality

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,3 +1,31 @@
 #include "main.h"
 
 /**
+ * _strspn - gets the length of a prefix substring
+ * @s: main string
+ * @accept: prefix substring
+ * Return: number of bytes in the initial segment of s
+ * which consist only of bytes from accept
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j, k;
+
+	i = 0;
+	for (; *s; s++)
+	{
+		k = i;
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (*s == accept[j])
+			{
+				i += 1;
+				break;
+			}
+		}
+		if  (i == k)
+			break;
+	}
+	return (i);
+}
