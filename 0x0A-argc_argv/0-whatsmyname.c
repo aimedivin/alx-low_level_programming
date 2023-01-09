ef9eb908ebbe74be0a8a978cba654d479ed10d58
aimedivin <aimedifi003@gmail.com>
2023-01-09 15:15:08 -0800

    0x0A. C - argc, argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints its name, followed by a new line
+ * @argc: Thhis in the number of command line argument
+ * @argv: This is an array name
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc * 0]);
+
+	return (0);
+}
